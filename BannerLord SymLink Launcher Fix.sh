7a5ef9e80ab9233c4913c66cbf8a31a747c35944
buckybrendan <77645273+buckybrendan@users.noreply.github.com>
2021-01-18 22:15:29 +0000

    Add files via upload

---
+++ BannerLord SymLink Launcher Fix.sh
@@ -0,0 +1,3 @@
+cd .local/share/Steam/steamapps/common/"Mount & Blade II Bannerlord"/bin/Win64_Shipping_Client
+mv TaleWorlds.MountAndBlade.Launcher.exe TaleWorlds.MountAndBlade.Launcher.exe.backup
+ln -s Bannerlord.Native.exe TaleWorlds.MountAndBlade.Launcher.exe
